8314ed9603c4c74f922555152b7b0e3da5e68449
Stefan Lukits <sediomyle@gmail.com>
2018-01-15 09:21:41 -0800

    gc-03-BasicDiff.tex

--- auto/gc-03-BasicDiff.el
+++ auto/gc-03-BasicDiff.el
@@ -4,12 +4,12 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("beamer" "xcolor=dvipsnames")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -33,6 +33,9 @@
     "eq:oomaenee"
     "eq:eeyeejeu"
     "eq:wuuxaise"
+    "eq:shoogaka"
+    "eq:ohniegae"
+    "eq:ohchoacu"
     "eq:thahchae"
     "eq:quaipahn"
     "eq:mitahrei"
