efa5eeec5a9c2fde34864ba822885b035592b48e
Stefan Lukits <sediomyle@gmail.com>
2018-01-24 08:12:08 -0800

    gc-06-ChainRule.tex

---
+++ auto/gc-06-ChainRule.el
@@ -0,0 +1,61 @@
+(TeX-add-style-hook
+ "gc-06-ChainRule"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "xcolor=dvipsnames")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "teachbeamer")
+   (LaTeX-add-labels
+    "eq:ciedaeme"
+    "eq:ohquailo"
+    "eq:seemaxah"
+    "eq:iebieluk"
+    "eq:eeghaphe"
+    "eq:quanoefe"
+    "eq:oozeexei"
+    "eq:faegeehi"
+    "eq:kooteiju"
+    "eq:oochahph"
+    "eq:bongaeza"
+    "eq:ooquonge"
+    "eq:iejafaic"
+    "eq:aepuaxai"
+    "eq:prf"
+    "eq:tieteiph"
+    "eq:iewaebef"
+    "eq:sivahzuw"
+    "eq:zeejaixu"
+    "eq:iecheixo"
+    "eq:zigaewai"
+    "eq:beetulae"
+    "eq:aekephii"
+    "eq:uijeabai"
+    "eq:ohzabeed"
+    "eq:pimexeiz"
+    "eq:oogheica"
+    "eq:ibagheab"
+    "eq:oboohoca"
+    "eq:choopaib"
+    "eq:uosiamei"
+    "eq:oshaiphu"
+    "eq:ciukaech"
+    "eq:oveagooy"
+    "eq:veeveema"
+    "eq:athaazui"
+    "eq:ahgoovim"
+    "eq:gaidaime"
+    "eq:vaixohga"
+    "eq:iefeuvae"))
+ :latex)
+
