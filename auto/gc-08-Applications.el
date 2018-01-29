e65dced2d27290b2c5fded6d598106a74b60774a
Stefan Lukits <sediomyle@gmail.com>
2018-01-29 09:07:30 -0800

    gc-07-HigherOrder.tex

---
+++ auto/gc-08-Applications.el
@@ -0,0 +1,32 @@
+(TeX-add-style-hook
+ "gc-08-Applications"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "xcolor=dvipsnames")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "teachbeamer")
+   (LaTeX-add-labels
+    "eq:zeipouqu"
+    "eq:phosulee"
+    "eq:otienaix"
+    "eq:kupeivae"
+    "eq:uquuthae"
+    "eq:sohngiel"
+    "eq:lauwutho"
+    "eq:chiowezo"
+    "eq:sayeeziu"
+    "eq:anuvuama"
+    "eq:iemahbec"))
+ :latex)
+
