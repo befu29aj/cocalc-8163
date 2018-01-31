68d558ab51c2470e5787384b964b129dffdeb20c
Stefan Lukits <sediomyle@gmail.com>
2018-01-31 10:35:43 -0800

    gc-lab-07-derivatives.tex

---
+++ auto/gc-lab-07-derivatives.el
@@ -0,0 +1,20 @@
+(TeX-add-style-hook
+ "gc-lab-07-derivatives"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("xcolor" "table")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "enumerate"
+    "syllogism"
+    "october"
+    "xcolor")
+   (TeX-add-symbols
+    "CourseName"
+    "CourseNumber"))
+ :latex)
+
