221bdee1458eb45e72607103cffabc7ca8178cc3
Stefan Lukits <sediomyle@gmail.com>
2018-02-07 12:23:24 -0800

    gc-08-Applications.tex

--- auto/gc-08-Applications.el
+++ auto/gc-08-Applications.el
@@ -3,14 +3,14 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("beamer" "xcolor=dvipsnames")))
-   (add-to-list 'LaTeX-verbatim-environments-local "alltt")
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-environments-local "alltt")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -41,6 +41,14 @@
     "eq:chiowezo"
     "eq:sayeeziu"
     "eq:anuvuama"
-    "eq:iemahbec"))
+    "eq:iemahbec"
+    "eq:egaireil"
+    "eq:iehahphi"
+    "eq:oothaich"
+    "eq:duusecho"
+    "eq:sahtuodo"
+    "eq:oophacuz"
+    "eq:roohahou"
+    "eq:eelajaim"))
  :latex)
 
