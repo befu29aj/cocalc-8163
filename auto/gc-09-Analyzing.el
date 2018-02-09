f2cd89e2ce2e3b8c8ca128f7d33fdc38b28aec45
Stefan Lukits <sediomyle@gmail.com>
2018-02-09 13:12:11 -0800

    gc-09-Analyzing.tex

--- auto/gc-09-Analyzing.el
+++ auto/gc-09-Analyzing.el
@@ -3,13 +3,14 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("beamer" "xcolor=dvipsnames")))
+   (add-to-list 'LaTeX-verbatim-environments-local "alltt")
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -26,6 +27,11 @@
     "eq:aweefahx"
     "eq:ebukatio"
     "eq:inaidimo"
+    "eq:xxyxx1"
+    "eq:xxyxx2"
+    "eq:xxyxx3"
+    "eq:xxyxx4"
+    "eq:xxyxx5"
     "eq:peimoojo"
     "eq:maugeish"
     "eq:cheodieh"
@@ -42,11 +48,6 @@
     "eq:uufiexah"
     "eq:opeemuix"
     "eq:oisheith"
-    "eq:xohhafoe"
-    "eq:xxyxx1"
-    "eq:xxyxx2"
-    "eq:xxyxx3"
-    "eq:xxyxx4"
-    "eq:xxyxx5"))
+    "eq:xohhafoe"))
  :latex)
 
