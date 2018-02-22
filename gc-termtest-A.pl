c680f594895036e4220e24d97b5ae1eedc93b0de
Stefan Lukits <sediomyle@gmail.com>
2018-02-21 16:59:31 -0800

    gc-09-Analyzing.tex

--- gc-termtest-A.pl
+++ gc-termtest-A.pl
@@ -1,5 +1,9 @@
 use List::Util 'shuffle';
 
+print "The problem with Newton's Cooling Law is too difficult, not knowing the initial temperature.\n";
+print "As an alternative I suggest Example 6 in Thomas' Calculus, page 508.\n";
+print "You may also want to replace the limit question with something easier.\n";
+
 srand;
 
 $aufgabe[0]=sub {
