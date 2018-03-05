9d504a0a68cbb81e52ae8c64c7a55753bf1e2ef0
Stefan Lukits <sediomyle@gmail.com>
2018-03-05 08:56:23 -0800

    gc-11-OptimizationNewton.tex

--- auto/gc-09-Analyzing.el
+++ auto/gc-09-Analyzing.el
@@ -3,14 +3,14 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("beamer" "xcolor=dvipsnames")))
-   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
    (add-to-list 'LaTeX-verbatim-environments-local "alltt")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -45,6 +45,16 @@
     "eq:xxyxx5"
     "eq:xxyxx6"
     "eq:xxyxx7"
+    "eq:aiyeiyoo"
+    "eq:aziobaeg"
+    "eq:shahquua"
+    "eq:ohghoote"
+    "eq:ouwophai"
+    "eq:poqueebo"
+    "eq:deiquoot"
+    "eq:xoonahju"
+    "eq:uabaevoo"
+    "eq:water"
     "eq:jaithean"
     "eq:ukohyaej"
     "eq:loocootu"
