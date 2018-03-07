e05157489903f8e8f227856f187de321c9bcfcad
Stefan Lukits <sediomyle@gmail.com>
2018-03-07 10:40:49 -0800

    gc-lab-11-lhopital.tex

---
+++ auto/gc-lab-11-lhopital.el
@@ -0,0 +1,23 @@
+(TeX-add-style-hook
+ "gc-lab-11-lhopital"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("xcolor" "table")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "enumerate"
+    "syllogism"
+    "october"
+    "xcolor")
+   (TeX-add-symbols
+    '("aufgabe" 0))
+   (LaTeX-add-labels
+    "eq:aiyeiyoo")
+   (LaTeX-add-counters
+    "aufg"))
+ :latex)
+
