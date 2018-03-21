a7d9f3f1c5bcc693ccec644c20dc8eee5e849c4b
Stefan Lukits <sediomyle@gmail.com>
2018-03-21 15:19:06 -0700

    gc-termtest-B.pl

--- gc-termtest-B.pl
+++ gc-termtest-B.pl
@@ -1,11 +1,6 @@
 # gc-termtest-B.pl
 use List::Util 'shuffle';
 
-print "The problem with Newton's Cooling Law is too difficult, not knowing the initial temperature.\n";
-print "As an alternative I suggest Example 6 in Thomas' Calculus, page 508.\n";
-print "You may also want to replace the limit question with something easier.\n";
-print "There is an alternative term test in gc-termtest-A-davidworthen.tex.\n";
-
 srand;
 
 $aufgabe[0]=sub {
